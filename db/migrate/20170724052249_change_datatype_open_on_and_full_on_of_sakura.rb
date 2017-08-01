7214d21e53331f094930b5ce33dcbd7512bd76d1
Swan Match <swan_match@yahoo.co.jp>
2017-08-01 14:12:57 +0900

    migration edti

--- db/migrate/20170724052249_change_datatype_open_on_and_full_on_of_sakura.rb
+++ db/migrate/20170724052249_change_datatype_open_on_and_full_on_of_sakura.rb
@@ -1,8 +1,8 @@
 class ChangeDatatypeOpenOnAndFullOnOfSakura < ActiveRecord::Migration
   def up
     ActiveRecord::Base.connection.execute("TRUNCATE sakuras;")
-    change_column :sakuras, :open_on, :integer
-    change_column :sakuras, :full_on, :integer
+    change_column :sakuras, :open_on, 'integer USING CAST(open_on AS integer)'
+    change_column :sakuras, :full_on, 'integer USING CAST(full_on AS integer)'
   end
   def down
     ActiveRecord::Base.connection.execute("TRUNCATE sakuras;")
